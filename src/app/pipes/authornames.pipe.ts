e515dbbe36eb1a14d8665045a2874689f7e989b6
yjama <ab1239189@gmail.com>
2022-11-18 23:29:19 -0500

    Final changes

---
+++ src/app/pipes/authornames.pipe.ts
@@ -0,0 +1,13 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Author} from '../books/model/book';
+
+@Pipe({
+  name: 'authornames'
+})
+export class AuthornamesPipe implements PipeTransform {
+
+  transform(value: Author[] | undefined): string {
+    if (value == null) return '';
+    return value.map((author) => `${author.firstName}, ${author.lastName}`).join(' <b>and</b> ');
+  }
+}
