048b31ae6fb84a63e621b9c4bc0d27fa5b2ca48b
yjama <ab1239189@gmail.com>
2022-11-18 23:17:54 -0500

    Final Lab changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './books/book/book.component';
+import { AuthornamesPipe } from './pipes/authornames.pipe';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+//YasinJ
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BooksComponent,
+    BookComponent,
+    AuthornamesPipe,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
